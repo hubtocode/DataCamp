eddab1bce4a5272b79b17d50e704b53c7a08ffe0
Wonderwoman2301 <49751051+hubtocode@users.noreply.github.com>
2019-11-12 22:54:46 -0500

    Update 06-which-arma-model-is-best.py

--- 22-introduction-to-time-series-analysis-in-python/05-putting-it-all-together/06-which-arma-model-is-best.py
+++ 22-introduction-to-time-series-analysis-in-python/05-putting-it-all-together/06-which-arma-model-is-best.py
@@ -1,7 +1,8 @@
 '''
 Which ARMA Model is Best?
-
-Fit the temperature data to an AR(1), AR(2), MA(1), and ARMA(1,1) and see which model is the best fit, using the AIC criterion.
+Recall from Chapter 3 that the Akaike Information Criterion (AIC) can be used to compare models with different numbers of parameters. It measures goodness-of-fit, but places a penalty on models with more parameters to discourage overfitting. Lower AIC scores are better.
+Fit the temperature data to an AR(1), AR(2), and ARMA(1,1) and see which model is the best fit, using the AIC criterion. The AR(2) and ARMA(1,1) models have one more parameter than the AR(1) has.
+The annual change in temperature is in a DataFrame chg_temp.
 
 INSTRUCTIONS
 100XP
@@ -13,16 +14,16 @@ Print the AIC value, found in res.aic
 from statsmodels.tsa.arima_model import ARMA
 
 # Fit the data to an AR(1) model and print AIC:
-mod = ARMA(chg_temp, order=(1,0))
-res = mod.fit()
-print("The AIC for an AR(1) is: ", res.aic)
+mod_ar1 = ARMA(chg_temp, order=(1, 0))
+res_ar1 = mod_ar1.fit()
+print("The AIC for an AR(1) is: ", res_ar1.aic)
 
 # Fit the data to an AR(2) model and print AIC:
-mod = ARMA(chg_temp, order=(2,0))
-res = mod.fit()
-print("The AIC for an AR(2) is: ", res.aic)
+mod_ar2 = ARMA(chg_temp, order=(2, 0))
+res_ar2 = mod_ar2.fit()
+print("The AIC for an AR(2) is: ", res_ar2.aic)
 
-# Fit the data to an MA(1) model and print AIC:
-mod = ARMA(chg_temp, order=(0,1))
-res = mod.fit()
-print("The AIC for an MA(1) is: ", res.aic)
+# Fit the data to an ARMA(1,1) model and print AIC:
+mod_arma11 = ARMA(chg_temp, order=(1, 1))
+res_arma11 = mod_arma11.fit()
+print("The AIC for an ARMA(1,1) is: ", res_arma11.aic)
